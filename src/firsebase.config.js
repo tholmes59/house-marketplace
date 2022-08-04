9d33cb4bf9b86658f2380c7f36b110e25379b2a1
Thomas <tholmes59@gmail.com>
2022-08-04 07:53:55 -0700

    set up firebase and add config file and include fet firestore

---
+++ src/firsebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnGIgdLtiXhVKCU1fXRiMLK0A96Nrcn84",
+  authDomain: "house-marketplace-app-d567c.firebaseapp.com",
+  projectId: "house-marketplace-app-d567c",
+  storageBucket: "house-marketplace-app-d567c.appspot.com",
+  messagingSenderId: "731754686844",
+  appId: "1:731754686844:web:c598d9e58c36b921ec5cf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
