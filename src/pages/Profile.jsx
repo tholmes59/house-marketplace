542842299e19b825ed6fdc0f7bbe44e715afcb64
Thomas <tholmes59@gmail.com>
2022-08-08 15:51:29 -0700

    add logout button and onsubmit function to loggout on click. also added form data for user name and meail in order to display information in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import {getAuth} from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 function Profile() {
-  const [user, setUser] = useState(null)
   const auth = getAuth()
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
 
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
-  return user ? <h1>{user.displayName}</h1> : 'Not logged in'
+  const {name, email} = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+  }
+
+  return (
+    <div className="profile">
+      <header className="rofileHeader">
+        <p className="pageHeader">My Profile</p>
+          <button type="button" className="logOut" onClick={onLogout}>
+            Logout
+          </button>
+        
+      </header>
+    </div>
+  );
 }
 
 export default Profile
