7af6b3108f45581e30d5a4f932dd821c94b0bf6e
Thomas <tholmes59@gmail.com>
2022-08-08 14:32:48 -0700

    create onsubmit function to send form login data to firebase and if credentials navigate to home page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {useState} from 'react'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visabilityIcon from '../assets/svg/visibilityIcon.svg'
+import { async } from '@firebase/util'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -21,13 +23,28 @@ function SignIn() {
     }))
   }
 
+  const onSubmit  = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      
+      if(userCredential.user){
+        navigate('/')
+      }   
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
