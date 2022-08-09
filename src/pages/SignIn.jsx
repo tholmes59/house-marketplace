a64c719445ae5e6bdf8496ab987e2fef267413cc
Thomas <tholmes59@gmail.com>
2022-08-09 15:36:39 -0700

    import oauth component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,7 +5,7 @@ import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {Link, useNavigate} from 'react-router-dom'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visabilityIcon from '../assets/svg/visibilityIcon.svg'
-import { async } from '@firebase/util'
+import OAuth from '../components/OAuth'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -81,7 +81,7 @@ function SignIn() {
             </button>
           </div>
         </form>
-        {/* OAuth */}
+        <OAuth/>
         <Link to='/sign-up' className='registerLink'>
           Sign Up
         </Link>
