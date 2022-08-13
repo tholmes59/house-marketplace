1dd936eb09a56f44b74c5468c8fc47a1e71a47b3
Thomas <tholmes59@gmail.com>
2022-08-13 09:15:44 -0700

    add slider component explore page to add a sliding, clickable view of each listing

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../components/Slider';
+
 
 
 function Explore() {
@@ -11,6 +13,7 @@ function Explore() {
         <p className="pageHeader">Explore</p>
       </header>
       <main>
+        <Slider/>
         <p className="exploreCategoryHeading">Categories</p>
         <div className="exploreCategories">
           <Link to="/category/rent">
