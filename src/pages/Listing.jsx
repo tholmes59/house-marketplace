7cf738eb619d51e3f812e55de6ce6748338f43c1
Thomas <tholmes59@gmail.com>
2022-08-13 09:11:37 -0700

    add swiper and leafelet map to listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,7 +6,12 @@ import { getAuth } from "firebase/auth";
 import { db } from "../firsebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
-import { async } from "@firebase/util";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import SwiperCore, {Navigation, Pagination, Scrollbar, A11y} from 'swiper'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+
 
 function Listing() {
   const [listing, setListing] = useState(null);
@@ -23,7 +28,6 @@ function Listing() {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log(docSnap.data());
         setListing(docSnap.data());
         setLoading(false);
       }
@@ -35,6 +39,20 @@ function Listing() {
 
   return (
     <main>
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: 'cover',
+              }}
+              className='swiperSlideDiv'
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
       <div
         className="shareIconDiv"
         onClick={() => {
@@ -79,16 +97,27 @@ function Listing() {
               ? `${listing.bathrooms} Bathrooms`
               : `${listing.bathrooms} Bathroom`}
           </li>
-          <li>
-            {listing.parking ? "Parking Included" : null}
-          </li>
-          <li>
-            {listing.furnished ? "Furnished" : null}
-          </li>
+          <li>{listing.parking ? "Parking Included" : null}</li>
+          <li>{listing.furnished ? "Furnished" : null}</li>
         </ul>
         <p className="listingLocationTitle">Location</p>
 
-        {/* MAP */}
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                <Popup>{listing.location}</Popup>
+                </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
