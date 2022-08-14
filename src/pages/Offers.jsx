80c497817ebaf1f18192b4576cbc6a97eef416fc
Thomas <tholmes59@gmail.com>
2022-08-14 10:27:37 -0700

    add pagination to load additional lisitngs as only the first 10 will be shown on load

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import {
   collection,
   getDocs,
@@ -10,7 +9,7 @@ import {
   limit,
   startAfter,
 } from "firebase/firestore";
-import { db } from "../firsebase.config";
+import { db } from '../firsebase.config'
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
@@ -18,8 +17,7 @@ import ListingItem from "../components/ListingItem";
 function Offers() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
-
-  const params = useParams();
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -32,6 +30,9 @@ function Offers() {
           limit(10)
         );
         const querySnap = await getDocs(q);
+
+        const lastVisible = querySnap.docs[querySnap.docs.length-1]
+        setLastFetchedListing(lastVisible)
 
         let listings = [];
         querySnap.forEach((doc) => {
@@ -48,6 +49,37 @@ function Offers() {
     };
     fetchListings();
   }, []);
+
+   //Pagination/load more
+   const onFetchMoreListings = async () => {
+    try {
+      const listingRef = collection(db, "listings");
+      const q = query(
+        listingRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length-1]
+      setLastFetchedListing(lastVisible)
+
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not find listings");
+    }
+  };
+
   return (
     <div className="category">
       <header>
@@ -70,6 +102,11 @@ function Offers() {
               ))}
             </ul>
           </main>
+          <br/>
+          <br/>
+          {lastFetchedListing && (
+              <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+          )}
         </>
       ) : (
         <p>There are no current offers</p>
